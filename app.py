233996bc1133a0e8aaaac811b1197e3664b2d601
esha-pandya0203 <epandya@andrew.cmu.edu>
2025-10-04 10:22:58 -0400

    Implemented data scraping code to be called upon startup. Postings are separated into files based on BLS soc code

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
+import asyncio 
+import sys 
 from modules.nav import navbar 
+from pages import dashboard, job_search
 
 st.set_page_config(page_title='Job Market Dashboard', page_icon='📊', layout='wide')
 st.title('Job Analyzer') 
 st.write('Explore salary trends, skills demand, and real job postings across North America.')
 
-from pages import dashboard, job_search
+if sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy()) 
+
+from utils.data_scraper import collect_all_job_postings, clear_processed_data
 
 def main(): 
     #webscrape all the data only the relevants ones reference bls_data dict 
     #store csv files in data/raw_data
+    with st.spinner("⏳ Collecting job postings. This may take up to 10 minutes. Please be patient..."):
+        clear_processed_data() 
+        collect_all_job_postings() 
 
     #populate application dashboards 
 
