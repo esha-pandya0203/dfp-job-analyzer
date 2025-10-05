75eb05be9a9b9847726da1c22faf3e737fdce9c7
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2025-10-05 14:20:44 -0400

    added imports

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from modules.nav import navbar 
 from utils.bls_data_scraper import fetch_bls_data, web_scrape_bls_employment_projections, pittsburgh_computer_wage_outlook
 from utils.data_loader import load_bls_data, load_pittsburgh_data, load_job_data
+from utils.data_scraper import collect_all_job_postings, clear_processed_data
 from data.bls_dict import bls_dict
 
 st.set_page_config(page_title='Job Market Dashboard', page_icon='📊', layout='wide')
@@ -15,9 +16,9 @@ from pages import dashboard
 def main(): 
     #webscrape all the data only the relevants ones reference bls_data dict 
     #store csv files in data/raw_data
-    with st.spinner("⏳ Collecting job postings. This may take up to 10 minutes. Please be patient..."):
-        clear_processed_data() 
-        collect_all_job_postings() 
+    # with st.spinner("⏳ Collecting job postings. This may take up to 10 minutes. Please be patient..."):
+    #     clear_processed_data() 
+    #     collect_all_job_postings() 
 
     #BLS DATA SCRAPE/API REQUESTS 
     fetch_bls_data()
